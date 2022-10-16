0c4e642712d9662461c19a73157e5fb02164ca6e
Mikhail Surin <surinmike@gmail.com>
2022-10-17 01:10:58 +0500

    18

--- 18.hs
+++ 18.hs
@@ -1,11 +1,3 @@
-import qualified Data.Text.Lazy as L
-
-unpackNumbers s = map (map readNumber) numbers
-    where
-        readNumber = (read :: String -> Int) . L.unpack
-        numbers = filter (/= [L.pack ""]) $ map (L.split (==' ')) lines
-        lines = L.split (=='\n') (L.pack s)
-
 maxPath lines = maximum $ foldl calcPath [0] lines
     where
         calcPath paths weights = zipWith (+) weights $ zipWith max first second
@@ -15,5 +7,5 @@ maxPath lines = maximum $ foldl calcPath [0] lines
 
 
 main = do
-    lines <- readFile "triangle"
-    print $ maxPath $ unpackNumbers lines
+    fileStr <- readFile "triangle"
+    print $ maxPath $ map (map (read::String->Int) . words) $ lines fileStr
