3bc500616364b4383c236432bedd15ad16f17f97
Mikhail Surin <surinmike@gmail.com>
2023-11-25 12:56:43 -0300

    add build instructions

--- flake.nix
+++ flake.nix
@@ -3,6 +3,13 @@
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    myghc = with pkgs; (
+      haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+          random
+          randomgen
+          parallel
+          split
+        ]));
   in with pkgs; {
 
     devShells.${system}.default = mkShell {
@@ -14,13 +21,36 @@
 
       packages = [
         neovim
-        (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
-          random
-          randomgen
-          parallel
-        ]))
+        myghc
         haskell-language-server
       ];
+    };
+
+    packages.${system}.default = stdenv.mkDerivation {
+      name = "euler";
+
+      buildInputs = [
+        myghc
+      ];
+
+      unpackPhase = ''
+        cp ${./.}/*.hs .
+      '';
+
+      buildPhase = ''
+        for f in *.hs; do
+          ghc $f -o $f.run || echo "failed $f"
+        done
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        for f in *.hs.run; do
+          cp $f $out/bin/$f;
+        done
+      '';
+
+      phases = [ "unpackPhase" "buildPhase" "installPhase" ];
     };
   };
 }
