f2743ec61b3e850c77f81cdcee730bcf5d7534d8
Mikhail Surin <surinmike@gmail.com>
2024-03-26 23:33:29 -0300

    82

--- 82.hs
+++ 82.hs
@@ -5,12 +5,11 @@ import Data.List.Split (splitOn)
 main = do
     file <- readFile "/home/ssmike/Downloads/0082_matrix.txt"
     let nums = map (map (read :: String -> Int) . splitOn ",") (lines file)
-    let width :: Int = length nums
-    let height :: Int = length $ head nums
-    let field = listArray (1, width) [listArray (1, height) row | row <- nums]
-    let firstColumn = [((i, 0), ListArrayGraphNode { nodeid = (i, 0), incidenceList = [((i+1, 0), 0) | i < height] ++ [((i, 1), field!i!1)] }) | i <- [1..width]]
-    let neighbours i j = [((i-1,j), field!(i-1)!j) | i > 1] ++ [((i+1,j), field!(i+1)!j) | i < width] ++ [((i,j+1), field!i!(j+1)) | j < height] -- [((i,j-1), field!i!(j-1)) | j > 1] ++ 
-    let fieldCells = [((i, j), ListArrayGraphNode { nodeid = (i, j), incidenceList = neighbours i j}) | i <- [1..width], j <- [1..height]]
-    let graph :: ListArrayGraph (Int, Int) Int = ArrayGraph $ array ((1, 0), (width, height)) $ firstColumn ++ fieldCells
-    print $ maximum $ map snd $ filter (\((i, j), w) -> j == height) $ dijkstra graph (1::Int, 0::Int) (0::Int)
+    let n = length nums
+    let field = listArray (1, n) [listArray (1, n) row | row <- nums]
+    let firstColumn = [((i, 0), ListArrayGraphNode { nodeid = (i, 0), incidenceList = [((i+1, 0), 0) | i < n] ++ [((i, 1), field!i!1)] }) | i <- [1..n]]
+    let neighbours i j = [((i-1,j), field!(i-1)!j) | i > 1] ++ [((i+1,j), field!(i+1)!j) | i < n] ++ [((i,j+1), field!i!(j+1)) | j < n] -- [((i,j-1), field!i!(j-1)) | j > 1] ++ 
+    let fieldCells = [((i, j), ListArrayGraphNode { nodeid = (i, j), incidenceList = neighbours i j}) | i <- [1..n], j <- [1..n]]
+    let graph :: ListArrayGraph (Int, Int) Int = ArrayGraph $ array ((1, 0), (n, n)) $ firstColumn ++ fieldCells
+    print $ minimum $ map snd $ filter (\((i, j), w) -> j == n) $ dijkstra graph (1::Int, 0::Int) (0::Int)
 
---
+++ GraphCheck.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
+import Test.QuickCheck.Arbitrary (Arbitrary(..), arbitrarySizedNatural)
+import Test.QuickCheck.Gen (oneof, sized, Gen, frequency)
+import Test.QuickCheck (quickCheckAll, chooseInt, generate, getSize, Positive(..))
+import Data.List (sort)
+import Data.Array (listArray, (!))
+import Debug.Trace
+import Graph
+
+
+prop_Bamboo cells = dists == dijkstra graph (1::Int) (0::Int)
+    where
+        dists = zip [1..] $ scanl (+) 0 cells
+        n = length cells
+        graph = ArrayGraph $ listArray (1, n+1) $ [ListArrayGraphNode {nodeid = i, incidenceList = [(i + 1, w)]} | (i, w) <- zip [1..n] cells ] ++ [ListArrayGraphNode { nodeid = n + 1, incidenceList = []} ]
+
+
+return []
+main = $quickCheckAll
