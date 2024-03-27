7648d21d10c746d441b4a4d2d8c79c42f0daf914
Mikhail Surin <surinmike@gmail.com>
2024-03-26 23:16:33 -0300

    wip 82

--- 82.hs
+++ 82.hs
@@ -1,64 +1,16 @@
-import qualified Data.Map as M
-import Data.Array (Ix, Array, range, bounds, (!))
-import qualified PQ
-
-class Eq nodeid => NodeCollection nodeid a where
-    nodes :: a -> [nodeid]
-
-class NodeCollection nodeid a => Graph nodeid edgemeta a where
-    edges :: a -> nodeid -> [(nodeid, edgemeta)]
-    edges graph node = do
-        neighbour <- nodes graph
-        let e = edge graph node neighbour 
-        meta <- unwrapMaybe e
-        return (neighbour, meta)
-        where
-            unwrapMaybe (Just x) = [x]
-            unwrapMaybe Nothing = []
-
-    edge :: a -> nodeid -> nodeid -> Maybe edgemeta
-    edge graph a b = if isempty foundEdges
-                        then Nothing
-                        else let [(_, meta)] = foundEdges in Just meta
-        where 
-            foundEdges = filter ((==b) . fst) $ edges graph a
-
-            isempty [] = True
-            isempty _ = False
-
-data ListArrayGraphNode i w = ListArrayGraphNode {nodeid :: i, incidenceList :: [(i, w)]}
-newtype ListArrayGraph i w = ArrayGraph (Array i (ListArrayGraphNode i w))
-
-instance Ix i => NodeCollection i (ListArrayGraph i w) where
-    nodes (ArrayGraph a) = range $ bounds a
-
-instance Ix i => Graph i w (ListArrayGraph i w) where
-    edges (ArrayGraph nodes) node = incidenceList $ nodes ! node
-
-
-dijkstra :: (Ord i, Ord w, Num w, Graph i w g) => g -> i -> [(i, w)]
-dijkstra graph start = answer . head $ dropWhile (not.final) $ iterate step seed
-    where
-        answer (a, _) = M.assocs a
-        final (distances, candidates) = (==0) $ PQ.heapSize candidates
-        seed = (M.empty, PQ.makeHeap (0, start))
-        step (distances, candidates)
-            | M.member minNode distances = skip
-            | otherwise = (M.insert minNode minDistance distances, updatedCandidates)
-            where
-                nextHeap = PQ.extractMin candidates
-                skip = (distances, nextHeap)
-
-                (minDistance, minNode) = PQ.getMin candidates
-
-                updates = [(minDistance + w, nodeid) | (nodeid, w) <- edges graph minNode]
-                updatedCandidates = foldl1 PQ.mergeHeaps (map PQ.makeHeap updates ++ [nextHeap])
-
---data Node idtype edgetype  = Node {nodeid :: idtype, edges :: [edgetype]};
---class Ix i => Graph
+import Graph
+import Data.Array (Ix, Array, range, bounds, (!), array, listArray)
+import Data.List.Split (splitOn)
 
 main = do
-    let heap = foldl1 PQ.mergeHeaps (map PQ.makeHeap [1..100])
-    print $ PQ.getMin heap
-    print $ PQ.getMin . PQ.extractMin $ heap
+    file <- readFile "/home/ssmike/Downloads/0082_matrix.txt"
+    let nums = map (map (read :: String -> Int) . splitOn ",") (lines file)
+    let width :: Int = length nums
+    let height :: Int = length $ head nums
+    let field = listArray (1, width) [listArray (1, height) row | row <- nums]
+    let firstColumn = [((i, 0), ListArrayGraphNode { nodeid = (i, 0), incidenceList = [((i+1, 0), 0) | i < height] ++ [((i, 1), field!i!1)] }) | i <- [1..width]]
+    let neighbours i j = [((i-1,j), field!(i-1)!j) | i > 1] ++ [((i+1,j), field!(i+1)!j) | i < width] ++ [((i,j+1), field!i!(j+1)) | j < height] -- [((i,j-1), field!i!(j-1)) | j > 1] ++ 
+    let fieldCells = [((i, j), ListArrayGraphNode { nodeid = (i, j), incidenceList = neighbours i j}) | i <- [1..width], j <- [1..height]]
+    let graph :: ListArrayGraph (Int, Int) Int = ArrayGraph $ array ((1, 0), (width, height)) $ firstColumn ++ fieldCells
+    print $ maximum $ map snd $ filter (\((i, j), w) -> j == height) $ dijkstra graph (1::Int, 0::Int) (0::Int)
 
---
+++ Graph.hs
@@ -0,0 +1,59 @@
+module Graph (NodeCollection, Graph, ListArrayGraphNode(..), ListArrayGraph(..), dijkstra) where
+
+import Data.Array (Ix, Array, range, bounds, (!), array, listArray)
+import qualified Data.Map as M
+import qualified PQ
+
+class Eq nodeid => NodeCollection nodeid a where
+    nodes :: a -> [nodeid]
+
+class NodeCollection nodeid a => Graph nodeid edgemeta a where
+    edges :: a -> nodeid -> [(nodeid, edgemeta)]
+    edges graph node = do
+        neighbour <- nodes graph
+        let e = edge graph node neighbour 
+        meta <- unwrapMaybe e
+        return (neighbour, meta)
+        where
+            unwrapMaybe (Just x) = [x]
+            unwrapMaybe Nothing = []
+
+    edge :: a -> nodeid -> nodeid -> Maybe edgemeta
+    edge graph a b = if isempty foundEdges
+                        then Nothing
+                        else let [(_, meta)] = foundEdges in Just meta
+        where 
+            foundEdges = filter ((==b) . fst) $ edges graph a
+
+            isempty [] = True
+            isempty _ = False
+
+data ListArrayGraphNode i w = ListArrayGraphNode {nodeid :: i, incidenceList :: [(i, w)]}
+    deriving Show
+newtype ListArrayGraph i w = ArrayGraph (Array i (ListArrayGraphNode i w))
+    deriving Show
+
+instance Ix i => NodeCollection i (ListArrayGraph i w) where
+    nodes (ArrayGraph a) = range $ bounds a
+
+instance Ix i => Graph i w (ListArrayGraph i w) where
+    edges (ArrayGraph nodes) node = incidenceList $ nodes ! node
+
+
+dijkstra :: (Ord i, Num w, Ord w, Graph i w g) => g -> i -> w -> [(i, w)]
+dijkstra graph start zero = answer . head $ dropWhile (not.final) $ iterate step seed
+    where
+        answer (a, _) = M.assocs a
+        final (distances, candidates) = (==0) $ PQ.heapSize candidates
+        seed = (M.empty, PQ.makeHeap (zero, start))
+        step (distances, candidates)
+            | M.member minNode distances = skip
+            | otherwise = (M.insert minNode minDistance distances, updatedCandidates)
+            where
+                nextHeap = PQ.extractMin candidates
+                skip = (distances, nextHeap)
+
+                (minDistance, minNode) = PQ.getMin candidates
+
+                updates = [(minDistance + w, nodeid) | (nodeid, w) <- edges graph minNode]
+                updatedCandidates = foldl1 PQ.mergeHeaps (map PQ.makeHeap updates ++ [nextHeap])
