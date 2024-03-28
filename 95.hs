ad2016a76bc55ea114f7bdf857545f552980389d
Mikhail Surin <surinmike@gmail.com>
2024-03-27 23:47:24 -0300

    95

---
+++ 95.hs
@@ -0,0 +1,51 @@
+import Euler (divisorsUpTo)
+import Data.Array ((!))
+import Data.Foldable (maximumBy)
+import Control.Monad (forM_)
+import qualified Data.Set as Set
+
+limit = 1*10^6
+
+divisors = divisorsUpTo limit
+
+multiples 1 = []
+multiples x = (firstDiv, power): multiples remainder
+    where 
+        firstDiv = let val = divisors ! x in min val x 
+        (power, remainder) = divBy firstDiv x
+
+        divBy x n
+            | n `mod` x /= 0 = (0, n)
+            | otherwise = let (pow, val) = divBy x (n `div` x)
+                          in (pow + 1, val)
+
+
+
+amicableOf x = product (map sumMuls $ multiples x) - x
+    where
+        sumMuls (p, d) = (p^(d+1) - 1) `div` (p - 1)
+
+exploreFrom start = maybe [] chainFrom chainStart
+    where
+        minFrom start seen
+            | start == 0 = Nothing
+            | start > limit = Nothing
+            | Set.member start seen = Just start
+            | otherwise = minFrom (amicableOf start) (Set.insert start seen)
+
+        chainStart = minFrom start Set.empty
+
+        chainFrom start = (start:) $ takeWhile (\x -> x /= start && x <= limit) $ drop 1 $ iterate amicableOf start
+
+lenFrom = length . exploreFrom
+
+main = do
+    print $ lenFrom 1064
+    print $ lenFrom 12496
+    let allChains = filter (/=[]) $ map exploreFrom [2..limit]
+    --let allChains = map exploreFrom [2..limit]
+    let keys = map (\xs -> (length xs, xs)) allChains
+    let maxKey = maximum keys
+    forM_ (map (minimum.snd) $ filter (\(x, y) -> x == fst maxKey) keys) print
+
+
