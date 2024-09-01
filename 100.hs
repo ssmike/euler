91a73560b8c1b2cbb581adf265c3bac1a7d6deee
Mikhail Surin <surinmike@gmail.com>
2024-09-01 16:34:30 +0300

    intersect lists

--- 100.hs
+++ 100.hs
@@ -1,20 +1,25 @@
-import Euler (integralSqrt)
+import Euler (integralSqrt, intersectSorted)
 import Control.Monad (forM_)
 
 checkNumber x = m * (m + 1) == x
     where
         m = integralSqrt x
     
-chain = map (\x -> x * (x - 1)) [10^12+1..]
+chainFrom x = map (\x -> x * (x - 1)) [x..]
+
+bound = 10^12
+
+chain = chainFrom $ bound+1
+chainHalf = map (*2) $ chainFrom $ integralSqrt (bound^2`div`2)
 
 filteredChain = filter (checkNumber . (`div`2))
 
 main = do
-    print $ checkNumber $ 21 * 20
-    print $ take 10 $ filter even chain
-    let mults = filter (checkNumber . (`div` 2)) $ filter even chain
-    let blues = map ((+1) . integralSqrt . (`div`2)) mults
-    forM_ blues print
-    --print $ map ((+1) . integralSqrt . (`div`2)) mults
-    --print $ map ((+1) . integralSqrt) $ take 20 filteredChain
+    --print $ checkNumber $ 21 * 20
+    --print $ take 10 $ filter even chain
+    --let mults = filter (checkNumber . (`div` 2)) $ filter even chain
+    --let blues = map ((+1) . integralSqrt . (`div`2)) mults
+    --forM_ blues print
+    let nums = intersectSorted chain chainHalf
+    forM_ nums $ \x -> print (1 + integralSqrt x, 1 + integralSqrt (x`div`2))
     
