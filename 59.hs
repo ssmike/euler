572208f3e2a89bb356203ea1c461dbe5a98f2dad
Mikhail Surin <surinmike@gmail.com>
2024-12-07 15:25:24 +0300

    59

--- 59.hs
+++ 59.hs
@@ -5,15 +5,19 @@ import Data.Bits (xor)
 xrchr :: Char -> Char -> Char
 xrchr x y = toEnum $ fromEnum x `xor` fromEnum y
 
-allChrs = enumFromTo 'a' 'z' ++ enumFromTo 'A' 'Z' ++ " "
+allChrs = enumFromTo 'a' 'z' ++ enumFromTo 'A' 'Z' ++ " ;:,.'?-!()" ++ enumFromTo '0' '9'
 allNums = map fromEnum allChrs
-chooseDigits vars = filter cond $ map toEnum [0..255] :: [Char]
+
+chooseDigits vars = filter cond $ map fromEnum $ enumFromTo 'a' 'z'
     where
-        cond var = and [(var `xrchr` toEnum base) `elem` allChrs | base <- vars]
+        cond var = and [(var `xor` toEnum base) `elem` allNums | base <- vars]
 
 main = do
+    -- print $ xrchr 'A' 'k'
     file <- readFile "/home/ssmike/Downloads/0059_cipher.txt"
     let nums = zip (map (`mod`3) [0..]) $ map (read :: String -> Int) $ splitOn "," file
+    print $ sort nums
+    -- print $ map ((toEnum :: Int -> Char) . snd) nums
     let alls =  map (map snd) $ groupBy (\a b -> fst a == fst b) $ sort nums
-    print alls
+    -- print alls
     print $ map chooseDigits alls
