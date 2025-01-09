3dc63bf668c250588409bd7af1b4a58a1fb66808
Mikhail Surin <ssmike@ydb.tech>
2025-01-09 16:30:10 +0300

    wip

--- 165.hs
+++ 165.hs
@@ -1,3 +1,4 @@
+import Control.Monad (guard)
 ss = drop 1 $ iterate (\s -> (s * s) `mod` 50515093) 290797
 
 ts :: [Integer]
@@ -11,6 +12,20 @@ instance (Num a) => Num (Vec a) where
     negate (Vec x y) = Vec (-x) (-y)
 
 scalMult (Vec x1 y1) (Vec x2 y2) = (x1 * x2) + (y1 + y2)
+vecMult (Vec x1 y1) (Vec x2 y2) = (x1 * y2) - (y1 * x2)
+
+haveIntersection (p1, p2) (s1, s2) = not parallel && pLineCrossesS && sLineCrossesP
+    where
+        parallel = vecMult (p2 - p1) (s2 - s1) == 0
+        differentSigns a b
+            | a == 0 = True
+            | b == 0 = True
+            | otherwise = signum a /= signum b
+
+        pLineCrossesS = vecMult (p2 - p1) (s2 - p1) `differentSigns` vecMult (p2 - p1) (s1 - p1)
+        sLineCrossesP = vecMult (s2 - s1) (p2 - s1) `differentSigns` vecMult (s2 - s1) (p1 - s1)
+
+intersectLines (p1, p2) (s1, s2) = error "undefined"
 
 pointPairs = foldSegments $ foldPoints ts 
     where
@@ -21,6 +36,14 @@ pointPairs = foldSegments $ foldPoints ts
         foldPoints ts = Vec x y : foldPoints (drop 2 ts)
             where
                 [x, y] = take 2 ts
+
+
+explorePairs pts = do
+    first <- pts
+    second <- pts
+    guard $ first /= second
+    guard $ haveIntersection first second
+    return $ intersectLines first second
 
 
 main = do
